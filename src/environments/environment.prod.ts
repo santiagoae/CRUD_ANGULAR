a31c71cd2182a32f9b82d721f64275ae7930438a
santiagoae <santiago_avendano82162@elpoli.edu.co>
2022-07-26 08:51:35 -0500

    v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyANcj-SdbKHAFHqxoV5BxxSTij8-swKr_M",
+    authDomain: "angularfb-17360.firebaseapp.com",
+    projectId: "angularfb-17360",
+    storageBucket: "angularfb-17360.appspot.com",
+    messagingSenderId: "1010929210376",
+    appId: "1:1010929210376:web:a4770f7c1b00fb56252dbd",
+    measurementId: "G-TVEVSDT4JJ"
+  }
 };
